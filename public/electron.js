dc199fc11dd3daedce9ccdb36f0c58c01f5a1e48
Abduquddus Azizov <abduquddus.08@mail.ru>
2023-02-22 01:13:32 +0500

    new version

--- public/electron.js
+++ public/electron.js
@@ -6,7 +6,6 @@ const isDev = require("electron-is-dev");
 let mainWindow;
 
 const createWindow = () => {
-    // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -23,27 +22,19 @@ const createWindow = () => {
             : `file://${path.join(__dirname, '../build/index.html')}`
     )
 
-    // Open the DevTools.
     if (isDev) {
         mainWindow.webContents.openDevTools({ mode: "detach" });
-        // require('react-devtools-electron');
-    };
-
-    if (!isDev) {
-        autoUpdater.checkForUpdates();
-    };
+    }
 
     autoUpdater.checkForUpdates();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+    if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates();
+})
+
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
         app.quit();
@@ -51,8 +42,6 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
@@ -62,13 +51,11 @@ autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
     const dialogOpts = {
         type: 'info',
         buttons: ['Ok'],
-        title: 'Application Update',
+        title: 'Update Available',
         message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail: 'A new version is being downloaded.'
-    }
-    dialog.showMessageBox(dialogOpts, (response) => {
-
-    });
+        detail: 'A new version download started. The app will be restarted to install the update.'
+    };
+    dialog.showMessageBox(dialogOpts);
 })
 
 autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
@@ -79,6 +66,9 @@ autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
         message: process.platform === 'win32' ? releaseNotes : releaseName,
         detail: 'A new version has been downloaded. Restart the application to apply the updates.'
     };
+
+    autoUpdater.quitAndInstall();
+
     dialog.showMessageBox(dialogOpts).then((returnValue) => {
         if (returnValue.response === 0) autoUpdater.quitAndInstall()
     })
